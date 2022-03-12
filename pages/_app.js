a4c0dd0462f2bd8752afebcad8c840ac3f0fb075
Dhruv Prajapati <rp170854@gmail.com>
2022-03-13 00:01:18 +0530

    favicons add

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,12 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <meta name="google-site-verification" content="atvxsQuLP4Lz-yso578GWX3uNwvch15-_3qNoMaXocI" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest"></link>
     </Head>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
     <Component {...pageProps} />
